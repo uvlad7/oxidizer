3edf02206d915af8513db84f276695911d0ec482
uvlad7 <uvlad7@gmail.com>
2025-05-30 01:35:44 +0300

    bundle gem

---
+++ lib/oxidizer.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "oxidizer/version"
+require_relative "oxidizer/oxidizer"
+
+module Oxidizer
+  class Error < StandardError; end
+  # Your code goes here...
+end
