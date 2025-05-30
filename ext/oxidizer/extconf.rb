df44f1603b8db7595d3d6fd91a395fdc4850c3b9
uvlad7 <uvlad7@gmail.com>
2025-05-30 03:17:09 +0300

    prepare for pyo3

--- ext/oxidizer/extconf.rb
+++ ext/oxidizer/extconf.rb
@@ -3,4 +3,6 @@
 require "mkmf"
 require "rb_sys/mkmf"
 
-create_rust_makefile("oxidizer/oxidizer")
+create_rust_makefile("oxidizer/oxidizer") do |r|
+  r.features = %w[magnus]
+end
--- ext/oxidizer/src/lib.rs
+++ ext/oxidizer/src/lib.rs
@@ -1,9 +1,12 @@
+#[cfg(feature = "magnus")]
 use magnus::{function, prelude::*, Error, Ruby};
 
+#[cfg(feature = "magnus")]
 fn hello(subject: String) -> String {
     format!("Hello from Rust, {subject}!")
 }
 
+#[cfg(feature = "magnus")]
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("Oxidizer")?;
